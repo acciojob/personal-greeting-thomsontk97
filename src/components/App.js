1427c8924fb264c1953ed3bae97d86ea48ab9e41
Thomson T Kakkanattu <thomsontk237@gmail.com>
2023-07-22 12:55:34 +0000

    Msg

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let [name,setName] = useState("");
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>       Enter your name:
+</p>
+    <input type="text" onChange={(e) => setName("Hello " + e.target.value + "!")}/>
+    <p>{name}</p>
     </div>
   )
 }
